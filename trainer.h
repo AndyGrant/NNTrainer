14574283eb0243766a697e05e4b2b93cfe48de8d
Andrew Grant <andrew@grantnet.us>
2020-11-03 01:10:17 -0500

    Fix clang warnings over EOF

--- trainer.h
+++ trainer.h
@@ -60,5 +60,3 @@ Sample *load_samples(const char *fname, int length);
 void load_sample(FILE *fin, Sample *sample);
 
 void update_network(Optimizer *opt, Network *nn, Gradient **grads, Batch *batch);
-
-/**************************************************************************************************************/
